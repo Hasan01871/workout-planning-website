e46aa3cf6c08f11ad7e5960141582d80907d83ab
Hasan01871 <akash0163014@gmail.comgit config --global user.name>
2026-09-24 23:07:24 +0700

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,8 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
+    data-theme="light"
+
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
